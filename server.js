0e67964e5272a61c0b4d296cf3130925dc024cc5
Adrian Gamarra <gamarraadrian200@gmail.com>
2025-09-15 08:15:19 -0300

    Update port to 3000 to avoid conflicts

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Security middleware
 app.use(helmet({
